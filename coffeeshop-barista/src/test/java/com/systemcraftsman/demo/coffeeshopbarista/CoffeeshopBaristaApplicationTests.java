317c91b9c3415ac3f6370f6faf1c437c8b12aa3e
mabulgu <abulgu@redhat.com>
2023-10-14 01:03:03 +0300

    init projects

---
+++ coffeeshop-barista/src/test/java/com/systemcraftsman/demo/coffeeshopbarista/CoffeeshopBaristaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.systemcraftsman.demo.coffeeshopbarista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeshopBaristaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ coffeeshop-service/src/test/java/com/systemcraftsman/demo/coffeeshopservice/CoffeeshopServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.systemcraftsman.demo.coffeeshopservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeshopServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
